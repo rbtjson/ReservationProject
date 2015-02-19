eb337270a26b80e1b9d084728549f4c913a9c4dd
rbtjson <rbt.json@hotmail.com>
2015-02-19 21:28:18 +0100

    First react implementation

---
+++ js/reservation.calendar.js
@@ -0,0 +1,8 @@
+/*
+ * reservation.calendar.js
+ * Calendar for Reservation Project
+ */
+ React.render(
+   React.createElement('h1', null, 'Hello, world!'),
+   document.getElementById('example')
+ );
