112ca3063dc7839d5977a8c51ab71ea7194f012f
rbtjson <rbt.json@hotmail.com>
2015-02-20 21:06:00 +0100

    javascript react table

--- js/reservation.calendar.js
+++ js/reservation.calendar.js
@@ -2,7 +2,67 @@
  * reservation.calendar.js
  * Calendar for Reservation Project
  */
- React.render(
-   React.createElement('h1', null, 'Hello, world!'),
-   document.getElementById('example')
- );
+
+/*
+ var child1 = React.createElement('li', null, 'First Text Content');
+ var child2 = React.createElement('li', null, 'Second Text Content');
+ var root = React.createElement('ul', { className: 'my-list' }, child1, child2);
+
+ React.render( root, document.getElementById('calendar') );
+
+ */
+ // React.render( React.createElement('h1', null, 'Hello, world!'), document.getElementById('calendar') );
+ /*
+ <table>
+  <tr>
+    <th class="calendar-weekday">Monday</th>
+    <th class="calendar-weekday">Tuesday</th>
+  </tr>
+  <tr>
+    <td class="calendar-element">3</td>
+    <td class="calendar-element">4</td>
+  </tr>
+  <tr>
+    <td class="calendar-element">5</td>
+    <td class="calendar-element">6</td>
+  </tr>
+</table>
+*/
+
+
+
+var Table = React.createClass({
+
+  render: function render() {
+    var _self = this;
+
+    var thead = React.DOM.thead({},
+      React.DOM.tr({},
+        this.props.cols.map(function (col) {
+          return React.DOM.th({}, col);
+        })));
+
+        var tbody = this.props.rows.map(function (row) {
+          return React.DOM.tr({},
+            _self.props.cols.map(function (col) {
+              return React.DOM.td({}, row[col] || "");
+            }));
+          });
+
+          return React.DOM.table({}, [thead, tbody]);
+        }
+
+      });
+
+      var container = document.getElementById('calendar');
+
+      var tableModel = {
+        cols: ["Monday", "Tuesday"],
+        rows: [
+          {"Monday": "1", "Tuesday": "2",},
+          {"Monday": "3", "Tuesday": "4"}
+          ],
+
+      }
+
+      React.renderComponent(Table(tableModel), container);
