daed356521dbac7a48ba947bf6f98ae7be0f9211
rbtjson <rbt.json@hotmail.com>
2015-03-23 21:04:12 +0100

    Click event handler for he grid

--- js/simple_grid.js
+++ js/simple_grid.js
@@ -6,13 +6,33 @@ simpleGrid = function () {
     var gridPage;
     var initialized = false;
 
+    function renderGrid() {
+        console.log("renderGrid");
+    }
+
     return {
         init : function (page) {
             if (!initialized) {
                 gridPage = page;
 
-                $(gridPage).find('#el1').on('click', function (evt) {
-                    $(evt.target).toggleClass('grid_element_selected');
+                // Create the grid
+                renderGrid();
+
+                // Add on click event listener to the grid
+                $(gridPage).find('#calendar_grid').on('click', function (evt) {
+                    evt.preventDefault();
+
+                    // Iterate all the child elements in the grid.
+                    // Toggle the selected class on the clicked element and
+                    // make sure that the selected class is removed form all the other elements
+                    $(gridPage).find('#calendar_grid').children().each(function( i ) {
+                       if(this.id == evt.target.id) {
+                           $(this).toggleClass("grid_element_selected");
+                        }
+                       else {
+                           $(this).removeClass("grid_element_selected");
+                       }
+                    });
                 });
 
                 initialized = true;
